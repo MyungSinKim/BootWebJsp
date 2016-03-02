05812826f5cab1b17bb53568bba9cdff82a7d439
dev_kyn <wizardkyn@naver.com>
2016-03-02 22:08:31 +0900

    initial

---
+++ src/main/java/com/example/web/BootWebJspApplication.java
@@ -0,0 +1,12 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootWebJspApplication.class, args);
+	}
+}
